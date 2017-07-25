3d6d6696527fa8021a18ccaecdf0de8dc0e1f226
Kjeld Schmidt <s_kjeld@hotmail.de>
2017-07-25 14:39:08 +0200

    Completed ESP8266 Example, which now sends a HTML response and has pseudo-interactive buttons with which a pin can be remote controlled.

--- ESP Attempts/src/main.cpp
+++ ESP Attempts/src/main.cpp
@@ -1,10 +1,10 @@
 
-#import "Arduino.h"
+#include "Arduino.h"
 #include <ESP8266WiFi.h>
 
 WiFiServer server(80);
 
-#define LED_PIN 2
+#define CONTROLLED_PIN D2
 
 void setup() {
     Serial.begin( 9600 );
@@ -14,6 +14,9 @@ void setup() {
     IPAddress server_ip = WiFi.softAPIP();
     Serial.print( "Server operating at IP ");
     Serial.println( server_ip );
+
+    pinMode( CONTROLLED_PIN, OUTPUT );
+    digitalWrite( CONTROLLED_PIN, HIGH );
 }
 
 void loop() {
@@ -21,6 +24,21 @@ void loop() {
     if ( !client ) {
         return;
     }
-
     Serial.println( "A device has connected!" );
+
+    String request = client.readStringUntil('\r');
+    if ( request.indexOf("HIGH") != -1 ) {
+        digitalWrite( CONTROLLED_PIN, HIGH );
+    } else if ( request.indexOf("LOW") != -1 ) {
+        digitalWrite( CONTROLLED_PIN, LOW );
+    }
+
+    String controlHTML = "HTTP/1.1 200 OK\r\n";
+    controlHTML += "Content-Type: text/html\r\n\r\n";
+    controlHTML += "<!DOCTYPE HTML>\r\n<html>\r\n";
+    controlHTML += "<button onclick=\"location.href = '/HIGH'\">HIGH</button>\r\n";
+    controlHTML += "<button onclick=\"location.href = '/LOW'\">LOW</button>\r\n";
+    controlHTML += "</html>\r\n";
+
+    client.print(controlHTML);
 }
