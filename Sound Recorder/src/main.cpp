8eccba90aee31f63b8f42872263269246e78957a
Kjeld Schmidt <contact@kjeld-schmidt.com>
2017-10-15 17:04:32 +0200

    Fixed sample ratio, improved file writing, not useable yet

--- Sound Recorder/src/main.cpp
+++ Sound Recorder/src/main.cpp
@@ -1,8 +1,17 @@
 #include <SPI.h>
 #include <SD.h>
 
-#define MIC_PIN A0
+// PHYSICAL CONSTANTS
+#define MICROS_PER_SECOND 1000000
 
+
+// PROGRAM PARAMETERS
+#define AUDIO_BUFFER_SIZE 200
+#define SAMPLE_FREQUENCY 8000
+
+
+// PINS
+#define MIC_PIN 0
 #define SIGNAL_LED 2
 
 // SD Pins
@@ -11,14 +20,34 @@
 #define MISO_PIN 12
 #define SCK_PIN 13
 
-byte audioData[1000];
+byte audioData[AUDIO_BUFFER_SIZE];
 size_t dataOffset = 0;
 unsigned long currentTime;
-unsigned long samplePeriod = 8000;
+unsigned long samplePeriod = MICROS_PER_SECOND / SAMPLE_FREQUENCY;
 unsigned long previousSampleTime;
 
 bool inErorrState = false;
 File audioFile;
+
+void writeNumToFile( uint32_t num) {
+    byte byte0 = num >> 24;
+    byte byte1 = num >> 16;
+    byte byte2 = num >> 8;
+    byte byte3 = num >> 0;
+
+    audioFile.write( byte0 );
+    audioFile.write( byte1 );
+    audioFile.write( byte2 );
+    audioFile.write( byte3 );
+}
+
+void writeNumToFile( uint16_t num) {
+    byte byte0 = num >> 8;
+    byte byte1 = num >> 0;
+
+    audioFile.write( byte0 );
+    audioFile.write( byte1 );
+}
 
 void createFile() {
     String potentialFileName = "test.txt";
@@ -38,22 +67,23 @@ void writeRiffHeader() {
     audioFile.print( "0000" );          // Chunk Size - needs to be set at the end of recording to Filesize - 8 Bytes
     audioFile.print( "WAVE" );          // WAVE - End Chunk
     audioFile.print( "fmt " );          // fmt - Begin Subchunk 1
-    audioFile.print( (uint32_t) 16 );   // Subchunk 1 Size, constant
-    audioFile.print( (uint16_t) 1 );    // AudioFormat, Linear quantization
-    audioFile.print( (uint16_t) 1 );    // Channel count
-    audioFile.print( (uint32_t) 8000 ); // Sample Rate 
-    audioFile.print( (uint32_t) 8000 ); // Byte Rate
-    audioFile.print( (uint16_t) 1 );    // Block Align
-    audioFile.print( (uint16_t) 1 );    // BitsPerSample - End Subchunk 1
+    writeNumToFile( (uint32_t) 16 );    // Subchunk 1 Size, constant
+    writeNumToFile( (uint16_t) 1 );     // AudioFormat, Linear quantization
+    writeNumToFile( (uint16_t) 1 );     // Channel count
+    writeNumToFile( (uint32_t) 8000 );  // Sample Rate 
+    writeNumToFile( (uint32_t) 8000 );  // Byte Rate
+    writeNumToFile( (uint16_t) 1 );     // Block Align
+    writeNumToFile( (uint16_t) 1 );     // BitsPerSample - End Subchunk 1
     audioFile.print( "data" );          // data - Begin Subchunk 2
     audioFile.print( "0000");           // Total Size of Data not including any of the header. Needs to be set to Filesize - 44 Bytes
+    audioFile.flush();
 }
 
 void finalizeFile() {
     audioFile.seek( 4 ); // Goto Chunk Size
-    audioFile.print( audioFile.size() - 8 );
+    writeNumToFile( audioFile.size() - 8 );
     audioFile.seek( 40 );
-    audioFile.print( audioFile.size() - 44 );
+    writeNumToFile( audioFile.size() - 44 );
     audioFile.close();
 }
 
@@ -67,10 +97,7 @@ void setup() {
     if ( !SD.begin( CS_PIN ) ) {
         inErorrState = true;
         return;
-    } 
-    File logger = SD.open( "duh.txt", FILE_WRITE );
-    logger.println( "Fuck this shit I'm out" );
-    logger.close();
+    }
     initializeFile();
 }
 
@@ -84,21 +111,21 @@ void loop() {
         }
     }
 
-    currentTime = millis();
+    currentTime = micros();
 
-    if ( currentTime < (unsigned long) 60*1000 && previousSampleTime + samplePeriod <= currentTime ) {
+    if ( currentTime < 60UL*1000000UL && previousSampleTime + samplePeriod <= currentTime ) {
         audioData[ dataOffset ] = analogRead( MIC_PIN );
         ++dataOffset;
-        previousSampleTime = millis();
+        previousSampleTime = currentTime;
     }
 
-    if ( dataOffset == 1000 ) {
+    if ( dataOffset == AUDIO_BUFFER_SIZE ) {
         dataOffset = 0;
-        audioFile.write( audioData, 1000 );
+        audioFile.write( audioData, AUDIO_BUFFER_SIZE );
         audioFile.flush();
     }
 
-    if ( currentTime > (unsigned long) 61*1000 ) {
+    if ( currentTime > 61UL * 1000000UL ) {
         finalizeFile();
         digitalWrite( SIGNAL_LED, HIGH );
         while ( true );
