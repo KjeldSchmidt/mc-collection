8ba3381727953ae09634ede197884aa91d2d612f
Kjeld Schmidt <contact@kjeld-schmidt.com>
2018-01-14 23:36:59 +0100

    Wicked Lightning: Color is read and displayed correctly

--- Wicked Lightring/src/main.cpp
+++ Wicked Lightring/src/main.cpp
@@ -1,27 +1,35 @@
 #include <Arduino.h>
 #include <FastLED.h>
 
-#define REDPIN A0
-#define GREENPIN A1
+#define REDPIN A1
+#define GREENPIN A0
 #define BLUEPIN A3
 
-unsigned char red1 = 0;
-unsigned char green1 = 0;
-unsigned char blue1 = 0;
+#define NUM_LEDS 16
+#define DATA_PIN 9
+
+CRGB leds[NUM_LEDS];
+
+CRGB color1;
+
+uint8_t redReading = 0;
+uint8_t greenReading = 0;
+uint8_t blueReading = 0;
 
 void setup() {
     Serial.begin(9600);
+    FastLED.addLeds<WS2811, DATA_PIN, RGB>(leds, NUM_LEDS);
 }
 
 void loop() {
-    red1 = analogRead(REDPIN) / 4;
-    green1 = analogRead(GREENPIN) / 4;
-    blue1 = analogRead(BLUEPIN) / 4;
-    Serial.print("Red: ");
-    Serial.println(red1, DEC);
-    Serial.print("Green: ");
-    Serial.println(green1, DEC);
-    Serial.print("Blue: ");
-    Serial.println(blue1, DEC);
-    delay(1000);
+    redReading = analogRead(REDPIN) / 4;
+    greenReading = analogRead(GREENPIN) / 4;
+    blueReading = analogRead(BLUEPIN) / 4;
+
+    color1 = CRGB(redReading, greenReading, blueReading);
+
+    for (int i = 0; i < NUM_LEDS; ++i) {
+        leds[i] = color1;
+    }
+    FastLED.show();
 }
