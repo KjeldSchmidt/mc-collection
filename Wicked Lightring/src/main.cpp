043d0065e11141087595c06d771dfe87b7996b80
Kjeld Schmidt <contact@kjeld-schmidt.com>
2018-02-25 09:12:46 +0100

    Started working on switching colors

--- Wicked Lightring/src/main.cpp
+++ Wicked Lightring/src/main.cpp
@@ -1,9 +1,11 @@
 #include <Arduino.h>
 #include <FastLED.h>
 
-#define REDPIN A1
-#define GREENPIN A0
-#define BLUEPIN A3
+#define RED_PIN A1
+#define GREEN_PIN A0
+#define BLUE_PIN A3
+
+#define SWITCH_PIN 4
 
 #define NUM_LEDS 16
 #define DATA_PIN 9
@@ -16,16 +18,28 @@ uint8_t redReading = 0;
 uint8_t greenReading = 0;
 uint8_t blueReading = 0;
 
+int mode = 0;
+int prevMode = 0;
+
 void setup() {
+    pinMode(RED_PIN, INPUT);
+    pinMode(GREEN_PIN, INPUT);
+    pinMode(BLUE_PIN, INPUT);
+    pinMode(SWITCH_PIN, OUTPUT);
     Serial.begin(9600);
+    delay(1000);
     FastLED.addLeds<WS2811, DATA_PIN, RGB>(leds, NUM_LEDS);
 }
 
 void loop() {
-    redReading = analogRead(REDPIN) / 4;
-    greenReading = analogRead(GREENPIN) / 4;
-    blueReading = analogRead(BLUEPIN) / 4;
+    prevMode = mode;
+    mode = digitalRead( SWITCH_PIN );
+    Serial.println(mode, DEC);
+    delay(200);
 
+    redReading = analogRead(RED_PIN) / 4;
+    greenReading = analogRead(GREEN_PIN) / 4;
+    blueReading = analogRead(BLUE_PIN) / 4;
     color1 = CRGB(redReading, greenReading, blueReading);
 
     for (int i = 0; i < NUM_LEDS; ++i) {
