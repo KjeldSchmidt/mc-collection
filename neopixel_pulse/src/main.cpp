72f419fa5bc23f0f911519aae435beb7b710e558
Kjeld Schmidt <s_kjeld@hotmail.de>
2018-04-16 17:18:06 +0200

    Added neopixelring roulette and neopixel pulse.

---
+++ neopixel_pulse/src/main.cpp
@@ -0,0 +1,46 @@
+#import <Arduino.h>
+#import <FastLED.h>
+
+#define NUM_LEDS 144
+#define LED_DATA_PIN 9
+
+#define MAXIMUM_BRIGHTNESS_PER_CHANNEL 50
+#define LOOP_WAIT_TIME 50
+
+#define RED_WAVES_PER_STRIP 3
+#define GREEN_WAVES_PER_STRIP 2
+#define BLUE_WAVES_PER_STRIP 1
+
+long lastLoopTime = 0;
+
+char red;
+char green;
+char blue;
+
+char offset = 0;
+
+CRGBArray<NUM_LEDS> leds;
+
+void setup() {
+	FastLED.addLeds<NEOPIXEL, LED_DATA_PIN>(leds, NUM_LEDS);
+}
+
+char calc(int freq, char pos) {
+	return (char) (cos( PI*freq*pos / NUM_LEDS ) + 1) * MAXIMUM_BRIGHTNESS_PER_CHANNEL/2;
+}
+
+void loop() {
+	if ( millis() > lastLoopTime + LOOP_WAIT_TIME ) {
+		for ( int i = 0; i < NUM_LEDS; ++i ) {
+			red = calc( RED_WAVES_PER_STRIP,i );
+			green = calc( GREEN_WAVES_PER_STRIP,i );
+			blue = calc( BLUE_WAVES_PER_STRIP,i );
+
+			leds[ (i + offset) % NUM_LEDS  ] = CRGB( red, green, blue );
+			++offset;
+		}
+
+		FastLED.show();
+		lastLoopTime = millis();
+	}
+}
