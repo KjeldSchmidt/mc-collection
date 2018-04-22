975fc6e9e6961a5433a91ed97700a952ff7c2381
Kjeld Schmidt <contact@kjeld-schmidt.com>
2018-04-22 22:43:09 +0200

    Neopixel pulse perfected as a basis for other things.

--- neopixel_pulse/src/main.cpp
+++ neopixel_pulse/src/main.cpp
@@ -4,21 +4,16 @@
 #define NUM_LEDS 16
 #define LED_DATA_PIN 9
 
-#define MAXIMUM_BRIGHTNESS_PER_CHANNEL 5
-#define LOOP_WAIT_TIME 3000
+#define MAXIMUM_BRIGHTNESS_PER_CHANNEL 10 // On a scale from 1-255, of course. Actual value may be very slightly higher.
+#define SPEED_OF_LIGHT 1000 // Speed of animation in 
 
-#define RED_WAVES_PER_STRIP 2
-#define GREEN_WAVES_PER_STRIP 3
-#define BLUE_WAVES_PER_STRIP 4
-
-long lastLoopTime = 0;
+#define RED_WAVES_PER_STRIP 2 // Experiment with these
+#define GREEN_WAVES_PER_STRIP 5 // Experiment with these
+#define BLUE_WAVES_PER_STRIP 3 // Experiment with these
 
 unsigned char red;
 unsigned char green;
 unsigned char blue;
-
-unsigned char offset = 0;
-
 CRGBArray<NUM_LEDS> leds;
 
 void setup() {
@@ -26,22 +21,17 @@ void setup() {
 }
 
 unsigned char calc(int freq, float pos) {
-	return (char) (cos( pos*PI*freq*2 / NUM_LEDS ) * MAXIMUM_BRIGHTNESS_PER_CHANNEL) + (MAXIMUM_BRIGHTNESS_PER_CHANNEL + 1);
+	return (char) ((cos( pos*PI*freq*2 / NUM_LEDS ) * MAXIMUM_BRIGHTNESS_PER_CHANNEL) + (MAXIMUM_BRIGHTNESS_PER_CHANNEL + 1))/2;
 }
 
 void loop() {
 	unsigned long time = millis();
 	for ( int i = 0; i < NUM_LEDS; ++i ) {
-		red = calc( RED_WAVES_PER_STRIP, (float) i - ( (float) ( time % LOOP_WAIT_TIME ) / (float) LOOP_WAIT_TIME ) );
-		green = calc( GREEN_WAVES_PER_STRIP, (float) i - ( (float) ( time % LOOP_WAIT_TIME ) / (float) LOOP_WAIT_TIME ) );
-		blue = calc( BLUE_WAVES_PER_STRIP, (float) i - ( (float) ( time % LOOP_WAIT_TIME ) / (float) LOOP_WAIT_TIME ) );
+		red = calc( RED_WAVES_PER_STRIP, (float) i - ( (float) ( time ) / (float) SPEED_OF_LIGHT ) * RED_WAVES_PER_STRIP );
+		green = calc( GREEN_WAVES_PER_STRIP, (float) i - ( (float) ( time ) / (float) SPEED_OF_LIGHT ) * GREEN_WAVES_PER_STRIP );
+		blue = calc( BLUE_WAVES_PER_STRIP, (float) i - ( (float) ( time ) / (float) SPEED_OF_LIGHT ) * BLUE_WAVES_PER_STRIP );
 
-		leds[ ( i + offset ) % NUM_LEDS ] = CRGB( red, green, blue );
+		leds[i] = CRGB( red, green, blue );
 	}
 	FastLED.show();
-
-	if ( millis() > lastLoopTime + LOOP_WAIT_TIME ) {
-		lastLoopTime = time;
-		++offset;
-	}
 }
