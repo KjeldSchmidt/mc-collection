58cf7fb9379c1667dd06bae9f8c781d8e9600c87
Kjeld Schmidt <s_kjeld@hotmail.de>
2018-10-20 09:45:05 +0200

    Comments

--- ESP Attempts/src/main.cpp
+++ ESP Attempts/src/main.cpp
@@ -1,15 +1,19 @@
+/*
+ * This sketch starts a local wifi server, which serves a web page with two buttons
+ * These buttons can be used to pull a pin high or low, controlling, for example, an LED
+ */
 
-#include "Arduino.h"
+#include "Arduino.h" // Not neccesary when using the Aruino IDE
 #include <ESP8266WiFi.h>
 
 WiFiServer server(80);
 
-#define CONTROLLED_PIN D2
+#define CONTROLLED_PIN D2 // Normal numbrs of arduino pins do not apply for for the ESP8266
 
 void setup() {
     Serial.begin( 9600 );
     WiFi.mode( WIFI_AP );
-    WiFi.softAP( "D1 Mini", "12345678" );
+    WiFi.softAP( "D1 Mini", "12345678" ); // Wifi name and password
     server.begin();
     IPAddress server_ip = WiFi.softAPIP();
     Serial.print( "Server operating at IP ");
@@ -20,12 +24,13 @@ void setup() {
 }
 
 void loop() {
-    WiFiClient client = server.available();
-    if ( !client ) {
-        return;
+    WiFiClient client = server.available(); // Check if a request to the server is made
+    if ( !client ) { // If not, do nothing.
+        return; 
     }
     Serial.println( "A device has connected!" );
 
+    // If the requested url contains "HIGH" or "LOW", set the conrolled pin accordingly
     String request = client.readStringUntil('\r');
     if ( request.indexOf("HIGH") != -1 ) {
         digitalWrite( CONTROLLED_PIN, HIGH );
@@ -33,6 +38,7 @@ void loop() {
         digitalWrite( CONTROLLED_PIN, LOW );
     }
 
+    // Output web page content
     String controlHTML = "HTTP/1.1 200 OK\r\n";
     controlHTML += "Content-Type: text/html\r\n\r\n";
     controlHTML += "<!DOCTYPE HTML>\r\n<html>\r\n";
