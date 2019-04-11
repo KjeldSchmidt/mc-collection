1bf33c653ce2b061c14b154da52d50a3ea203b1d
Kjeld Schmidt <contact@kjeld-schmidt.com>
2019-04-11 12:55:04 +0200

    Made stepper motor run in full step.

--- Stepper Motor Attempts/src/main.cpp
+++ Stepper Motor Attempts/src/main.cpp
@@ -1,8 +1,12 @@
 #include <Arduino.h>
+#include "LowPower.h"
 
-#define STEP_DELAY 5
+#define STEP_DELAY 7
+#define RPM 1/120
+#define STEP_PER_TURN 2048
 
-int motorPins[] = {8, 9, 10, 11};
+
+int motorPins[] = {4, 5, 6, 7};
 int currentActiveMotorPin = 0;
 
 void stepForwardBy( int steps ) {
@@ -11,8 +15,9 @@ void stepForwardBy( int steps ) {
         digitalWrite( motorPins[1], LOW);
         digitalWrite( motorPins[2], LOW);
         digitalWrite( motorPins[3], LOW);
-        currentActiveMotorPin = (currentActiveMotorPin + 1) % 4;
+        currentActiveMotorPin = (currentActiveMotorPin + 1) % 4;    
         digitalWrite( motorPins[ currentActiveMotorPin ], HIGH);
+        digitalWrite( motorPins[ (currentActiveMotorPin + 1) % 4 ], HIGH);
         delay( STEP_DELAY );
     }
 }
@@ -23,9 +28,14 @@ void stepBackwardBy( int steps ) {
         digitalWrite( motorPins[1], LOW);
         digitalWrite( motorPins[2], LOW);
         digitalWrite( motorPins[3], LOW);
+        
         currentActiveMotorPin = currentActiveMotorPin - 1;
         if ( currentActiveMotorPin < 0 ) currentActiveMotorPin = 3;
+        int secondActivePin = currentActiveMotorPin - 1;
+        if ( secondActivePin < 0 ) secondActivePin = 3;
+        
         digitalWrite( motorPins[ currentActiveMotorPin ], HIGH);
+        digitalWrite( motorPins[ secondActivePin ], HIGH);
         delay( STEP_DELAY );
     }
 }
@@ -35,11 +45,18 @@ void setup() {
   pinMode( motorPins[1], OUTPUT);
   pinMode( motorPins[2], OUTPUT);
   pinMode( motorPins[3], OUTPUT);
-  stepForwardBy( 100 );
-  delay( 500 );
-  stepBackwardBy( 200 );
+}
+
+void motor_off() {
+  digitalWrite( motorPins[0], LOW);
+  digitalWrite( motorPins[1], LOW);
+  digitalWrite( motorPins[2], LOW);
+  digitalWrite( motorPins[3], LOW); 
 }
 
 void loop() {
-
+  LowPower.powerDown(SLEEP_4S, ADC_OFF, BOD_OFF); 
+  stepBackwardBy(1);
+  motor_off();
 }
+
