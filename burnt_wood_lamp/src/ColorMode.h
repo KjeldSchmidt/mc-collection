1a0b27a9633870df1dd8b3e490e5ca19a03fef8f
Kjeld Schmidt <contact@kjeld-schmidt.com>
2020-12-01 19:05:36 +0100

    Showing a distinct difference in color between the strips

--- burnt_wood_lamp/src/ColorMode.h
+++ burnt_wood_lamp/src/ColorMode.h
@@ -16,14 +16,12 @@ public:
 class FireMode : public ColorMode {
 public:
 	uint16 Update( CRGB *leds_out, uint8_t num_leds ) override {
-		CRGB fire = CRGB( 40, 10, 0 );
-		long r;
+		CRGB fire = CRGB( 20, 5, 0 );
+		CRGB value = leds_out[ 0 ];
+		long r = random( 40 );
+		value += fire;
+		value -= CRGB( r, r / 4, 0 );
 		for ( uint8_t i = 0; i < num_leds; i++ ) {
-			CRGB value = leds_out[ i ];
-			r = random( 40 );
-
-			value += fire;
-			value += CRGB( r, r / 4, 0 );
 			leds_out[ i ] = value;
 		}
 		return 50;
