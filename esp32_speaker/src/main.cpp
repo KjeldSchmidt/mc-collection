1c06e57c11d22aff61df11b1d816820422e1ad5e
Laura Stronk <contact@kjeld-schmidt.com>
2021-08-27 19:37:29 +0200

    Simplified esp32_speaker to the essentials, i.e., nothing

--- esp32_speaker/src/main.cpp
+++ esp32_speaker/src/main.cpp
@@ -6,19 +6,10 @@
 
 void setup() {
 	// Set the speakerPin as an output
-	Serial.begin( 115200 );
 	dac_output_enable( speakerPin );
 
 }
 
 void loop() {
-	Serial.println( "255" );
-	dac_output_voltage( speakerPin, 255 );
-	delay( 3000 );
-	Serial.println( "128" );
-	dac_output_voltage( speakerPin, 128 );
-	delay( 3000 );
-	Serial.println( "0" );
-	dac_output_voltage( speakerPin, 0 );
-	delay( 3000 );
+	dac_output_voltage( speakerPin, 127 + 127 * sin( millis() / 10 ));
 }
