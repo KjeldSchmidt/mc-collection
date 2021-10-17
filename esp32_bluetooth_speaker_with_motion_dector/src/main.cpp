0877e6b6bcafe91230bd98f08a48c696d6a6d0dd
Kjeld Schmidt <contact@kjeld-schmidt.com>
2021-10-17 17:25:27 +0200

    Trigger Motion Detector on interrupt

--- esp32_bluetooth_speaker_with_motion_dector/src/main.cpp
+++ esp32_bluetooth_speaker_with_motion_dector/src/main.cpp
@@ -1,16 +1,24 @@
 #include <Arduino.h>
 #include "../lib/ESP32-A2DP-1.4.0/src/BluetoothA2DPSink.h"
 
+#define MOTION_DETECTION_PIN 17
+
 BluetoothA2DPSink a2dp_sink;
+
+void motion_detected() {
+	Serial.println( "Motion detected!" );
+}
 
 void setup() {
 	a2dp_sink.start( "Dschungelmusik" );
-	pinMode( 21, OUTPUT );
+	Serial.begin( 9600 );
+	pinMode( 17, INPUT );
+	attachInterrupt(
+			digitalPinToInterrupt( MOTION_DETECTION_PIN ),
+			motion_detected,
+			RISING
+	);
 }
 
 void loop() {
-	digitalWrite( 21, HIGH );
-	delay( 500 );
-	digitalWrite( 21, LOW );
-	delay( 500 );
 }
