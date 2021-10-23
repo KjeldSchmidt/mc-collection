65e20225119a2d3c0e536194e7151152942a48af
Kjeld Schmidt <contact@kjeld-schmidt.com>
2021-10-23 16:00:21 +0200

    Now playing from Bluetooth stream and SD card simultaneously.

---
+++ esp32_bluetooth_speaker_with_motion_dector/src/SoundPlayer.h
@@ -0,0 +1,36 @@
+//
+// Created by kjeld on 18/06/2020.
+//
+
+#ifndef VINYL_RECORD_PLAYER_SOUND_PLAYER_H
+#define VINYL_RECORD_PLAYER_SOUND_PLAYER_H
+
+#include <Arduino.h>
+#include "../.pio/libdeps/wemos_d1_mini32/MD_YX5300/src/MD_YX5300.h"
+
+class SoundPlayer {
+public:
+	SoundPlayer( Stream &stream );
+
+	void play_folder( uint8_t folder );
+
+	void begin();
+
+	bool check();
+
+	void play_next();
+
+	static uint16_t SERIAL_BPS;
+
+private:
+	void handle_message();
+
+	void play_current();
+
+	MD_YX5300 mp3;
+	uint8_t current_file_index = 0;
+	uint8_t current_folder_index = 0;
+};
+
+
+#endif //VINYL_RECORD_PLAYER_SOUND_PLAYER_H
--- esp32_bluetooth_speaker_with_motion_dector/src/main.cpp
+++ esp32_bluetooth_speaker_with_motion_dector/src/main.cpp
@@ -1,24 +1,52 @@
 #include <Arduino.h>
 #include "../lib/ESP32-A2DP-1.4.0/src/BluetoothA2DPSink.h"
+#include "../.pio/libdeps/wemos_d1_mini32/EspSoftwareSerial/src/SoftwareSerial.h"
+
+#ifndef USE_SOFTWARESERIAL
+#define USE_SOFTWARESERIAL 1   ///< Set to 1 to use SoftwareSerial library, 0 for native serial port
+#endif
+
+#include "SoundPlayer.h"
 
 #define MOTION_DETECTION_PIN 17
+#define SD_PLAYER_TX 18
+#define SD_PLAYER_RX 19
 
 BluetoothA2DPSink a2dp_sink;
+SoftwareSerial sdPlayerControlStream{ SD_PLAYER_RX, SD_PLAYER_TX };
+SoundPlayer player{ sdPlayerControlStream };
+
+volatile bool play = false;
 
 void motion_detected() {
 	Serial.println( "Motion detected!" );
+	play = true;
 }
 
 void setup() {
 	a2dp_sink.start( "Dschungelmusik" );
 	Serial.begin( 9600 );
+	sdPlayerControlStream.begin( SoundPlayer::SERIAL_BPS );
 	pinMode( 17, INPUT );
+
 	attachInterrupt(
 			digitalPinToInterrupt( MOTION_DETECTION_PIN ),
 			motion_detected,
 			RISING
 	);
+
+	player.begin();
 }
 
 void loop() {
+	delay( 2000 );
+	player.check();
+
+	Serial.println( "Looping" );
+
+	if ( play ) {
+		player.play_folder( 3 );
+		play = false;
+		Serial.println( "Playing in loop" );
+	}
 }
