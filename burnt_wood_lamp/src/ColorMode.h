7910f53957fd89347571c02fbe893f29cf2be6ce
Kjeld Schmidt <kjeld.schmidt@thoughtworks.com>
2022-02-05 14:55:50 +0100

    Add generic SingleColorBlink mode and refactor DiscoStrobo to use it

--- burnt_wood_lamp/src/ColorMode.h
+++ burnt_wood_lamp/src/ColorMode.h
@@ -579,6 +579,32 @@ private:
 	CRGB color_lower;
 };
 
+class SingleColorBlink : public ColorMode {
+public:
+	SingleColorBlink( const CRGB &color, uint16 blink_time ):
+		color( color ),
+		blink_time( blink_time ) {}
+
+	uint16 Update( CRGB *leds_out ) override {
+		static bool on = true;
+		for ( uint8_t i = 0; i < NUM_LEDS; i++ ) {
+			if ( on ) {
+				leds_out[ i ] = color;
+			} else {
+				leds_out[ i ] = CRGB::Black;
+			}
+		}
+
+		on = !on;
+
+		return blink_time;
+	}
+
+private:
+	CRGB color;
+	uint16 blink_time;
+};
+
 class LightsOut : public SingleColor {
 public:
 	LightsOut() : SingleColor( CRGB::Black ) {}
@@ -599,23 +625,12 @@ public:
 	}
 };
 
-class DiscoStrobo : public ColorMode {
+class DiscoStrobo : public SingleColorBlink {
 public:
-	uint16 Update( CRGB *leds_out ) override {
-		static bool on = true;
-		for ( uint8_t i = 0; i < NUM_LEDS; i++ ) {
-			if ( on ) {
-				leds_out[ i ] = CRGB( MAX_BRIGHTNESS_PER_CHANNEL, MAX_BRIGHTNESS_PER_CHANNEL,
-				                      MAX_BRIGHTNESS_PER_CHANNEL );
-			} else {
-				leds_out[ i ] = CRGB::Black;
-			}
-		}
-
-		on = !on;
-
-		return 50;
-	}
+	DiscoStrobo() : SingleColorBlink( 
+		CRGB(MAX_BRIGHTNESS_PER_CHANNEL, MAX_BRIGHTNESS_PER_CHANNEL, MAX_BRIGHTNESS_PER_CHANNEL),
+		50
+	) {}
 };
 
 class CityAtSundown : public DualColor {
