0c1e4513821c7bec646826978f4b21860f5f3776
Kjeld Schmidt <contact@kjeld-schmidt.com>
2023-04-09 18:21:17 +0200

    Revert "Add trigger function handler placeholders"
    This reverts commit abea3520f4aea848dbfbfc1823b444b1f80862e7.
    
    This is because this code belongs in `control-box/`, and does indeed
    live there as of the last commit

--- mpr121-sample/mpr121-sample/src/main.cpp
+++ mpr121-sample/mpr121-sample/src/main.cpp
@@ -12,9 +12,10 @@
 // IRQ and ADD can float
 Adafruit_MPR121 cap = Adafruit_MPR121();
 
-// create 10 arrays, each with 10 elements set to 0
-uint8_t lastReadings[10][10] = {0};
-uint8_t last_reading_pointer;
+// Keeps track of the last pins touched
+// so we know when buttons are 'released'
+uint16_t lasttouched = 0;
+uint16_t currtouched = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -31,60 +32,40 @@ void setup() {
     Serial.println("MPR121 not found, check wiring?");
     while (1);
   }
-  cap.writeRegister(MPR121_AUTOCONFIG0, 0x0B);
-
-  // correct values for Vdd = 3.3V
-  cap.writeRegister(MPR121_UPLIMIT, 200);     // ((Vdd - 0.7)/Vdd) * 256
-  cap.writeRegister(MPR121_TARGETLIMIT, 180); // UPLIMIT * 0.9
-  cap.writeRegister(MPR121_LOWLIMIT, 130);    // UPLIMIT * 0.65
   Serial.println("MPR121 found!");
 }
 
 void loop() {
-  // debugging info
+  // Get the currently touched pads
+  currtouched = cap.touched();
+  
+  for (uint8_t i=0; i<12; i++) {
+    // it if *is* touched and *wasnt* touched before, alert!
+    if ((currtouched & _BV(i)) && !(lasttouched & _BV(i)) ) {
+      Serial.print(i); Serial.println(" touched");
+    }
+    // if it *was* touched and now *isnt*, alert!
+    if (!(currtouched & _BV(i)) && (lasttouched & _BV(i)) ) {
+      Serial.print(i); Serial.println(" released");
+    }
+  }
+
+  // reset our state
+  lasttouched = currtouched;
+  
+  // debugging info, what
   Serial.print("\t\t\t\t\t\t\t\t\t\t\t\t\t 0x"); Serial.println(cap.touched(), HEX);
   Serial.print("Filt: ");
-  for (uint16_t i=0; i<10; i++) {
-    uint16_t currentReading = cap.filteredData(i);
-
-    // calculate the average of the last 10 readings
-    uint16_t average = 0;
-    for (uint8_t j=0; j<10; j++) {
-      average += lastReadings[i][j];
-    }
-
-
-    Serial.print(currentReading); Serial.print("\t");
-
+  for (uint8_t i=0; i<12; i++) {
+    Serial.print(cap.filteredData(i)); Serial.print("\t");
   }
   Serial.println();
-
-  delay(100);
+  Serial.print("Base: ");
+  for (uint8_t i=0; i<12; i++) {
+    Serial.print(cap.baselineData(i)); Serial.print("\t");
+  }
+  Serial.println();
+  
+  // put a delay so it isn't overwhelming
+  delay(2500);
 }
-
-// create ten void functions with no argument and populate triggerFunctions
-void triggerFunction0() { Serial.println("triggerFunction0"); }
-void triggerFunction1() { Serial.println("triggerFunction1"); }
-void triggerFunction2() { Serial.println("triggerFunction2"); }
-void triggerFunction3() { Serial.println("triggerFunction3"); }
-void triggerFunction4() { Serial.println("triggerFunction4"); }
-void triggerFunction5() { Serial.println("triggerFunction5"); }
-void triggerFunction6() { Serial.println("triggerFunction6"); }
-void triggerFunction7() { Serial.println("triggerFunction7"); }
-void triggerFunction8() { Serial.println("triggerFunction8"); }
-void triggerFunction9() { Serial.println("triggerFunction9"); }
-
-
-// create an array of ten function pointers to void functions with no arguments and fill it with triggerFunctions
-void (*triggerFunctions[10])() = {
-  triggerFunction0,
-  triggerFunction1,
-  triggerFunction2,
-  triggerFunction3,
-  triggerFunction4,
-  triggerFunction5,
-  triggerFunction6,
-  triggerFunction7,
-  triggerFunction8,
-  triggerFunction9
-};
